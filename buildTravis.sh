ca54606196c41f7e1ef41120e86d3de2350d2009
mraab <mraab@intershop.de>
2016-04-15 10:43:43 +0200

    add git status to check the status of the git clone

--- buildTravis.sh
+++ buildTravis.sh
@@ -7,17 +7,25 @@ export GRADLE_OPTS="-Dorg.gradle.daemon=true"
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+        git status
         ./gradlew test build -s
+        git status
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
+        git status
         ./gradlew -PrunOnCI=true test build :bintrayUpload :publishPlugins -s
+        git status
     fi
 else
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+        git status
         ./gradlew test build -s
+        git status
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
+        git status
         ./gradlew -PrunOnCI=true test build :bintrayUpload :publishPlugins -s
+        git status
     fi
 fi
