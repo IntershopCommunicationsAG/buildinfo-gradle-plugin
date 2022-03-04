8d2604f91058ff2211038fffafbfba5fc9eee5a7
Matthias Raab <M.Raab@intershop.de>
2022-03-04 16:36:46 +0100

    Add build scan plug

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+plugins {
+    id("com.gradle.enterprise").version("3.8.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
 rootProject.name = "buildinfo-gradle-plugin"
